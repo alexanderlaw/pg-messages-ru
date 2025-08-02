d1ac70d867f1ee33412365c5d2f8075778111814
Alexander Lakhin <exclusion@gmail.com>
2025-08-02 11:36:23 +0300

    Stop processing messages for v12

--- scatter-po.sh
+++ scatter-po.sh
@@ -45,7 +45,6 @@ prepare_src_messages 16 REL_16_STABLE
 prepare_src_messages 15 REL_15_STABLE
 prepare_src_messages 14 REL_14_STABLE
 prepare_src_messages 13 REL_13_STABLE
-prepare_src_messages 12 REL_12_STABLE
 (
 cd $CUR_MESSAGES_PATH
 $POLOGY_PATH/bin/posummit -v scatter
@@ -55,5 +54,4 @@ finalize_po 16
 finalize_po 15
 finalize_po 14
 finalize_po 13
-finalize_po 12
 
